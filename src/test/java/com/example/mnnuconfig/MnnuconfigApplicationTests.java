6e85fa23948492242ebf095ec9d4ff362570dad6
zzx <769394358@qq.com>
2020-09-02 20:06:20 +0800

    first

---
+++ src/test/java/com/example/mnnuconfig/MnnuconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mnnuconfig;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MnnuconfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
